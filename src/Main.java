1f1ecba462bd83cc1d76b1d19989b7aeaf910884
Vishnu <vishnu.ratnawat@gmail.com>
2022-09-29 08:52:16 +0530

    abcd

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+
+	public class Main {
+	int modelYear;
+	String 	modelName;
+	
+	public Main(int year, String name) {
+	
+	modelYear = year;
+	modelName = name;
+	
+	
+}	
+	public static void main(String[] args) {
+		Main myCar = new Main(2001, "Alto");
+		System.out.println(myCar.modelName + ": " + myCar.modelYear);
+		Test_1 myObj = new Test_1();
+		myObj.fullThrottle();
+		myObj.speed(240);
+		
+		
+		Person obj = new Person();
+		obj.setName("Ojas");
+		
+		System.out.println(obj.getName());
+	}
+
+}
